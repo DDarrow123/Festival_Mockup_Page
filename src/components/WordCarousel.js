8b8794fcc487a295ec638fc83412274dba779938
DDarrow123 <ddarrowphl@gmail.com>
2019-02-17 17:49:26 -0500

    started building out slider

--- src/components/WordCarousel.js
+++ src/components/WordCarousel.js
@@ -3,7 +3,29 @@ import React from "react";
 const WordCarousel = () => {
   return (
     <div className="carousel-container">
-      <h1 className="carousel-names">I'm the Carousel!</h1>
+      <div className="carousel-slider">
+        <div className="slide" id="slide-1">
+          One
+        </div>
+        <div className="slide" id="slide-2">
+          Minimal
+        </div>
+        <div className="slide" id="slide-3">
+          Festival:
+        </div>
+        <div className="slide" id="slide-4">
+          Cinema.
+        </div>
+        <div className="slide" id="slide-5">
+          Art.
+        </div>
+        <div className="slide" id="slide-6">
+          Music.
+        </div>
+        <div className="slide" id="slide-7">
+          Design
+        </div>
+      </div>
     </div>
   );
 };
