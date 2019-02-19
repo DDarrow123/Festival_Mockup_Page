b6671f19fb52c75f4d86b9c8113573daae25389b
DDarrow123 <ddarrowphl@gmail.com>
2019-02-18 21:51:10 -0500

    built out most of mockup except media queries

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import lightening from "../images/lightening.png";
 
 const NavBar = () => {
   return (
     <header className="nav-bar">
+      <img
+        className="lightening-bolt"
+        src={lightening}
+        alt="Lightening Bolt Image"
+      />
       <span>One Minimal Festival</span>
       <span className="nav-bar-title">
         <a href="#">Get tickets!</a>
--- src/components/WordCarousel.js
+++ src/components/WordCarousel.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 class WordCarousel extends Component {
+  state = {
+    slideOneAnimate: false,
+    slideTwoAnimate: false,
+    slideThreeAnimate: false
+  };
+
   componentDidMount() {
     let carouselContainer = document.querySelector(".carousel-container");
     document.querySelector(".carousel-slider").scrollLeft = -100;
@@ -8,12 +14,39 @@ class WordCarousel extends Component {
   }
 
   scrollHorizontally = e => {
-    // let carouselContainer = document.querySelector(".carousel-container");
     e = window.event || e;
     var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
-    document.querySelector(".carousel-slider").scrollLeft -= delta * 40; // Multiplied by 40 (-= delta * 40;)
+    document.querySelector(".carousel-slider").scrollLeft -= delta * 40; // Multiplied by 40
     console.log(document.querySelector(".carousel-container").scrollLeft);
     e.preventDefault();
+  };
+
+  hoverOn = e => {
+    let exp = e.target.id;
+    switch (exp) {
+      case "slide-4":
+        this.setState({ slideOneAnimate: true });
+        break;
+      case "slide-6":
+        this.setState({ slideTwoAnimate: true });
+        break;
+      case "slide-7":
+        this.setState({ slideThreeAnimate: true });
+    }
+  };
+
+  hoverOff = e => {
+    let exp = e.target.id;
+    switch (exp) {
+      case "slide-4":
+        this.setState({ slideOneAnimate: false });
+        break;
+      case "slide-6":
+        this.setState({ slideTwoAnimate: false });
+        break;
+      case "slide-7":
+        this.setState({ slideThreeAnimate: false });
+    }
   };
 
   render() {
@@ -29,16 +62,37 @@ class WordCarousel extends Component {
           <div className="slide" id="slide-3">
             Festival:
           </div>
-          <div className="slide" id="slide-4">
+          <div
+            className={
+              this.state.slideOneAnimate ? "slide text-focus-in" : "slide"
+            }
+            id="slide-4"
+            onMouseEnter={this.hoverOn}
+            onMouseLeave={this.hoverOff}
+          >
             Cinema.
           </div>
           <div className="slide" id="slide-5">
             Art.
           </div>
-          <div className="slide" id="slide-6">
+          <div
+            className={
+              this.state.slideTwoAnimate ? "slide jello-horizontal" : "slide"
+            }
+            id="slide-6"
+            onMouseEnter={this.hoverOn}
+            onMouseLeave={this.hoverOff}
+          >
             Music.
           </div>
-          <div className="slide" id="slide-7">
+          <div
+            className={
+              this.state.slideThreeAnimate ? "slide vibrate-1" : "slide"
+            }
+            id="slide-7"
+            onMouseEnter={this.hoverOn}
+            onMouseLeave={this.hoverOff}
+          >
             Design
           </div>
         </div>
